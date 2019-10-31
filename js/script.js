302375c58318ef10b038437c3a5f221718334288
Pék Gábor <gpek1980@gmail.com>
2019-10-31 22:34:20 +0100

    Ez már Bootstrap

--- js/script.js
+++ js/script.js
@@ -33,4 +33,69 @@
 
 }());
 
+// Gallery
 
+var activeFilter = 'all';
+
+$('.pp-filter-button').on('click', function (e) {
+    // remove btn-primary from all buttons first
+    $('.pp-filter-button').removeClass('btn-primary');
+    $('.pp-filter-button').addClass('btn-outline-primary');
+
+    // add btn-primary to active button
+    var button = $(this);
+    button.removeClass('btn-outline-primary');
+    button.addClass('btn-primary');
+    filterItems(button.data("filter"));
+    e.preventDefault();
+})
+
+function filterItems(filter) {
+    if (filter === activeFilter) {
+        return;
+    }
+
+    activeFilter = filter;
+    $('.pp-gallery .card').each(function () {
+        var card = $(this);
+        var groups = card.data("groups");
+        var show = false;
+        if (filter === 'all') {
+            show = true;
+        } else {
+            for (var i = 0; i < groups.length; i++) {
+                if (groups[i] === filter) {
+                    show = true;
+                }
+            }
+        }
+        // hide everything first
+        card.fadeOut(400);
+
+        setTimeout(function () {
+            if (show && !card.is(":visible")) {
+                card.fadeIn(400)
+            }
+        }, 500);
+    });
+}
+
+mybutton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
